f40d1c79dcb1cb57a0a7b51be3b380ad953ca245
ChunWun <94582200+ChunWun@users.noreply.github.com>
2022-05-20 20:45:33 +0800

    add error view

--- src/components/User/UserInput.js
+++ src/components/User/UserInput.js
@@ -1,75 +1,108 @@
 import { useState } from "react";
 import Button from "../Element/Button.js";
 import Card from "../UI/Card";
+import ErrorModel from "../UI/ErrorModel.js";
 import styles from "./UserInput.module.css";
 
 const UserInput = (props) => {
 
-    const [userData, setUserData] = useState({
-        name: '',
-        age: ''
-    })
+	const [errorData, setErrorData] = useState({
+		title: null,
+		content: null
+	});
 
-    const onInputNameHandler = (event) => {
-        setUserData((prevState) => {
-            return (
-                {
-                    ...prevState,
-                    name: event.target.value,
-                    age: userData.age
-                }
-            );
+	const [userData, setUserData] = useState({
+		name: '',
+		age: ''
+	})
 
-        })
-    }
+	const onInputNameHandler = (event) => {
+		setUserData((prevState) => {
+			return (
+				{
+					...prevState,
+					name: event.target.value,
+					age: userData.age
+				}
+			);
 
-    const onInputAgeHandler = (event) => {
-        setUserData((prevState) => {
-            return (
-                {
-                    ...prevState,
-                    name: userData.name,
-                    age: event.target.value
-                }
-            );
+		})
+	}
 
-        })
-    }
+	const onInputAgeHandler = (event) => {
+		setUserData((prevState) => {
+			return (
+				{
+					...prevState,
+					name: userData.name,
+					age: event.target.value
+				}
+			);
 
-    const clearData = () => {
-        setUserData({
-            name: '',
-            age: ''
-        })
-    }
+		})
+	}
 
-    const onSummitHandler = (event) => {
-        event.preventDefault();
+	const clearData = () => {
+		setUserData({
+			name: '',
+			age: ''
+		})
+	}
 
-        if (userData.name.trim().length === 0 || userData.age.trim().length < 0) {
-            console.log('invail');
-            return;
-        }
+	const checkLegitimacy = () => {
+		if (userData.name.trim().length === 0 || userData.age.trim().length === 0) {
+			setErrorData({
+				title: 'Hey',
+				content: 'Please enter name and age.'
+			})
+			return false;
+		}
 
-        props.addNewUser({
-            name: userData.name,
-            age: userData.age
-        });
-        clearData();
-    }
+		if (userData.age < 0) {
+			setErrorData({
+				title: 'Are you kidding me?',
+				content: 'Please enter one correct age number.'
+			})
+			return false;
+		}
+		return true;
 
-    return (
-        <form onSubmit={onSummitHandler}>
-            <Card className={styles.input}>
-                <label>Name</label>
-                <input type="text" value={userData.name} onChange={onInputNameHandler}></input>
+	}
 
-                <label>Age</label>
-                <input type="number" min={0} max={110} value={userData.age} onChange={onInputAgeHandler}></input>
-                <Button type="summit">Add</Button>
-            </Card>
-        </form>
-    );
+	const onConfirmErrorHandler = () => {
+		setErrorData({
+			title: null,
+			content: null
+		})
+	}
+
+	const onSummitHandler = (event) => {
+		event.preventDefault();
+		if (checkLegitimacy()) {
+			props.addNewUser({
+				name: userData.name,
+				age: userData.age
+			});
+			clearData();
+		};
+	}
+
+	return (
+		<div>
+			{errorData.title && <ErrorModel title={errorData.title} content={errorData.content} onConfirm={onConfirmErrorHandler}></ErrorModel>}
+			<form onSubmit={onSummitHandler}>
+				<Card className={styles.input}>
+					<label>Name</label>
+					<input type="text" value={userData.name} onChange={onInputNameHandler}></input>
+
+					<label>Age</label>
+					<input type="number" min={0} max={110} value={userData.age} onChange={onInputAgeHandler}></input>
+					<Button type="summit">Add</Button>
+				</Card>
+			</form>
+		</div >
+
+	);
 }
 
 export default UserInput;
